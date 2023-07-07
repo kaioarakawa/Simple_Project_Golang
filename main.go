ad6d1c8ea30792685b6b8682f72bf8d8f1285bbc
Kaio Arakawa <ex.kaio@intelbras.com.br>
2023-07-07 11:25:40 -0300

    First commit

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Car struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+var cars []Car
+
+func createCars() {
+	cars = append(cars, Car{Name: "Carro 1", Price: 100})
+	cars = append(cars, Car{Name: "Carro 2", Price: 200})
+	cars = append(cars, Car{Name: "Carro 3", Price: 300})
+}
+
+func main() {
+	createCars()
+	e := echo.New()
+	e.GET("/cars", getCars)
+	e.POST("/cars", createCar)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func getCars(c echo.Context) error {
+	return c.JSON(200, cars)
+}
+
+func createCar(c echo.Context) error {
+	car := new(Car)
+
+	if err := c.Bind(car); err != nil {
+		return err
+	}
+
+	cars = append(cars, *car)
+	saveCar(*car)
+
+	return c.JSON(200, cars)
+}
+
+func saveCar(car Car) error {
+	db, err := sql.Open("sqllite3", "cars.db")
+
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("INSERT INTO cars (name, price) VALUES ($1, $2)")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(car.Name, car.Price)
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
